25af4feacc93ff8ace203f0d63138380143bf3bf
AnushaSPatil <89085457+anushaspatil53@users.noreply.github.com>
2025-01-13 11:04:33 +0530

    Add files via upload

---
+++ Lab2.js
@@ -0,0 +1,53 @@
+// Variables 
+// --------------------------------------------------------
+// Declare variables for the following:
+//   Your full name.
+//   Your age.
+
+
+console.log("Hello world!");
+
+let name = "Anusha S P"
+console.log(name);
+var age = 24
+console.log(age);
+
+// Declare four variables without assigning values
+var a,b,c,d  
+// Declare four variables with assigning values
+a="Hi "
+b="Welcome"
+c="to"
+d="Websecurity"
+console.log(a,b,c,d);
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+// let first_name 
+// let last_name 
+// let marital_status
+// let country
+// let age
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+var first_name, last_name, marital_status, country, age
+// Declare two variables _myAge_ and _yourAge_ and assign them initial values and log to browser console.
+// Output:
+
+// ```sh
+// I am 25 years old.
+// You are 30 years old.
+
+// ``
+var _myAge_=25
+var _yourAge_=30
+console.log("I am",_myAge_,"years old.\nYou are",_yourAge_,"years old.");
+
+
+// Constant 
+// ------------------------------------------------------------------------------
+// Declare a variable using var, let, and const, and log their initial values to the console.
+var varValue = "var variable."
+let letValue = "let variable is block scoped and cannot be re-declared."
+const constValue = "Constant variable is block scoped and cannot be re-declared."
+
+// Try reassigning values to each variable.
+varValue="changing var variable."
+letValue=""
